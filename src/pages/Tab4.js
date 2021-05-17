0051f63547f13b885d463aa3b26010c1ba6bdb3f
Bob <48901347+BobTheSoftwareDeveloper@users.noreply.github.com>
2021-05-18 01:03:27 +1200

    Manual ESLint fixing of curernt files

--- src/pages/Tab4.js
+++ src/pages/Tab4.js
@@ -1,24 +1,29 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import React from "react";
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import ExploreContainer from "../components/ExploreContainer.tsx";
 
-const Tab4 = () => {
-  return (
-    <IonPage>
-      <IonHeader>
+const Tab4 = () => (
+  <IonPage>
+    <IonHeader>
+      <IonToolbar>
+        <IonTitle>Tab 1</IonTitle>
+      </IonToolbar>
+    </IonHeader>
+    <IonContent fullscreen>
+      <IonHeader collapse="condense">
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle size="large">Tab 4</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 4</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 4 page" />
-      </IonContent>
-    </IonPage>
-  );
-};
+      <ExploreContainer name="Tab 4 page" />
+    </IonContent>
+  </IonPage>
+);
 
 export default Tab4;
