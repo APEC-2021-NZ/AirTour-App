ab1630882be1a4ad87b03351bcfad58a2d26fbbd
NeedsSoySauce <feras.albaroudi@gmail.com>
2021-05-25 17:00:31 +1200

    Display Login on tabbar

--- src/components/shared/TabBar.js
+++ src/components/shared/TabBar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import {
     IonIcon,
     IonLabel,
@@ -14,33 +14,37 @@ import {
     chatboxOutline,
     personOutline,
 } from 'ionicons/icons'
+import { AuthContext } from '../AuthProvider'
 
-const TabBar = ({ children }) => (
-    <IonTabs>
-        {children}
-        <IonTabBar slot="bottom">
-            <IonTabButton tab="explore" href="/explore">
-                <IonIcon icon={searchOutline} />
-                <IonLabel>Explore</IonLabel>
-            </IonTabButton>
-            <IonTabButton tab="wishlist" href="/wishlist">
-                <IonIcon icon={heartOutline} />
-                <IonLabel>Wishlists</IonLabel>
-            </IonTabButton>
-            <IonTabButton tab="bookings" href="/bookings">
-                <IonIcon icon={peopleOutline} />
-                <IonLabel>Bookings</IonLabel>
-            </IonTabButton>
-            <IonTabButton tab="chats" href="/chats">
-                <IonIcon icon={chatboxOutline} />
-                <IonLabel>Chats</IonLabel>
-            </IonTabButton>
-            <IonTabButton tab="profile" href="/profile">
-                <IonIcon icon={personOutline} />
-                <IonLabel>Profile</IonLabel>
-            </IonTabButton>
-        </IonTabBar>
-    </IonTabs>
-)
+const TabBar = ({ children }) => {
+    const { isAuthenticated } = useContext(AuthContext)
+    return (
+        <IonTabs>
+            {children}
+            <IonTabBar slot="bottom">
+                <IonTabButton tab="explore" href="/explore">
+                    <IonIcon icon={searchOutline} />
+                    <IonLabel>Explore</IonLabel>
+                </IonTabButton>
+                <IonTabButton tab="wishlist" href="/wishlist">
+                    <IonIcon icon={heartOutline} />
+                    <IonLabel>Wishlists</IonLabel>
+                </IonTabButton>
+                <IonTabButton tab="bookings" href="/bookings">
+                    <IonIcon icon={peopleOutline} />
+                    <IonLabel>Bookings</IonLabel>
+                </IonTabButton>
+                <IonTabButton tab="chats" href="/chats">
+                    <IonIcon icon={chatboxOutline} />
+                    <IonLabel>Chats</IonLabel>
+                </IonTabButton>
+                <IonTabButton tab="profile" href="/profile">
+                    <IonIcon icon={personOutline} />
+                    <IonLabel>{isAuthenticated ? 'Profile' : 'Login'}</IonLabel>
+                </IonTabButton>
+            </IonTabBar>
+        </IonTabs>
+    )
+}
 
 export default TabBar
